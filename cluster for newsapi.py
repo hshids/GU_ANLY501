01e1c55280f737d62130b601b4070cd799dd68e5
hshids <91449798+hshids@users.noreply.github.com>
2021-10-15 00:34:53 -0400

    Update cluster for newsapi.py

--- cluster for newsapi.py
+++ cluster for newsapi.py
@@ -375,16 +375,53 @@ fig.show()
 
 
 
+#Look at best values for k using gap-statistics
+# Gap Statistic for K means
+def optimalK(data, nrefs, maxClusters=15):
+    """
+    Calculates KMeans optimal K using Gap Statistic 
+    Params:
+        data: ndarry of shape (n_samples, n_features)
+        nrefs: number of sample reference datasets to create
+        maxClusters: Maximum number of clusters to test for
+    Returns: (gaps, optimalK)
+    """
+    gaps = np.zeros((len(range(1, maxClusters)),))
+    resultsdf = pd.DataFrame({'clusterCount':[], 'gap':[]})
+    for gap_index, k in enumerate(range(1, maxClusters)):
+# Holder for reference dispersion results
+        refDisps = np.zeros(nrefs)
+# For n references, generate random sample and perform kmeans getting resulting dispersion of each loop
+        for i in range(nrefs):
+            
+            # Create new random reference set
+            randomReference = np.random.random_sample(size=data.shape)
+            
+            # Fit to it
+            km = KMeans(k)
+            km.fit(randomReference)
+            
+            refDisp = km.inertia_
+            refDisps[i] = refDisp
+# Fit cluster to original data and create dispersion
+        km = KMeans(k)
+        km.fit(data)
+        
+        origDisp = km.inertia_
+# Calculate gap statistic
+        gap = np.log(np.mean(refDisps)) - np.log(origDisp)
+# Assign this loop's gap statistic to gaps
+        gaps[gap_index] = gap
+        
+        resultsdf = resultsdf.append({'clusterCount':k, 'gap':gap}, ignore_index=True)
+    return (gaps.argmax() + 1, resultsdf)
 
+score_g, df = optimalK(df, nrefs=7, maxClusters=10)
+plt.plot(df['clusterCount'], df['gap'], linestyle='--', marker='o', color='b');
+plt.xlabel('K');
+plt.ylabel('Gap Statistic');
+plt.title('Gap Statistic vs. K means');
 
-
-
-
-
-
-
-
-# In[ ]:
 
 
 
